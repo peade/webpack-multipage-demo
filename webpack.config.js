7f5700b199448b2e6f48d8d975a7074b83cd507f
hefaliu <hefaliu@hotmail.com>
2018-02-01 22:25:44 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,33 @@ let debug = process.env.NODE_ENV !== 'production'
 let entries = getEntry('src/scripts/page/**/*.js', 'src/scripts/page/')
 let chunks = Object.keys(entries)
 
+let config = {
+    entry: entries,
+    output:{
+        path:path.join(__dirname,'dist'),
+        publicPath:'/static/',
+        filename:'scripts/[name].js',
+        chunkFilename:'scripts/[name].chunk.js?[chunkhash]'
+    },
+    module:{
+        loaders:[
+            {
+                test:'/\.css$/',
+                loader:ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    use:'css-loader'
+                })
+            },
+            {
+                test:/.less$/,
+                loader:ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    use:['css-loader', 'less-loader']
+                })
+            }
+        ]
+    }
+}
 
 
 
